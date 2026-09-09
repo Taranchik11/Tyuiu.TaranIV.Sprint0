7bb09be203f3fdc9f4f18a3379a09cf6958c7d0f
Taran72 <taranivan72@mail.ru>
2026-09-09 17:09:37 +0500

    Task2

---
+++ Tyuiu.TaranIV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.TaranIV.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.TaranIV.Sprint0.Task2.V0.Test
+{  
+    [TestClass]
+        public sealed class DataServiceTest
+        {
+            [TestMethod]
+            public void CheckGetMessageValid()
+            {
+                var name = "Иван";
+                var res = DataService.GetMessage(name);
+
+                Assert.AreEqual("Привет, Иван", res);
+            }
+        }
+}
+
+
+
+    
---
+++ Tyuiu.TaranIV.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,5 @@
+﻿using Tyuiu.TaranIV.Sprint0.Task2.V0.Lib;
+
+Console.WriteLine(DataService.GetMessage("Иван"));
+Console.ReadKey();
+
