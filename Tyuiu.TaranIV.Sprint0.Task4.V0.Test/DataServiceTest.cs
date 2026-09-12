46d106ed7a6cdda0fd071f16cc4b1f52ac44a8dc
Taran72 <taranivan72@mail.ru>
2026-09-12 16:44:25 +0500

    Task4

---
+++ Tyuiu.TaranIV.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.TaranIV.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.TaranIV.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionalVallid()
+        {
+            Assert.AreEqual(10, DataService.Additional(5, 5));
+        }
+
+        public void CheckedSubtractionVallid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+
+        public void CheckedMultiplicationVallid()
+        {
+            Assert.AreEqual(50, DataService.Subtraction(10, 5));
+        }
+
+        public void CheckedDivisionVallid()
+        {
+            Assert.AreEqual(3, DataService.Subtraction(9, 3));
+        }
+    }
+}
---
+++ Tyuiu.TaranIV.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,7 @@
+﻿using Tyuiu.TaranIV.Sprint0.Task4.V0.Lib;
+
+Console.WriteLine(DataService.Additional(1, 5));
+Console.WriteLine(DataService.Subtraction(10, 5));
+Console.WriteLine(DataService.Multiplication(2, 5));
+Console.WriteLine(DataService.Division(10, 2));
+Console.ReadLine();
